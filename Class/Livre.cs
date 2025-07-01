4dfc59dc78a6e5e4537aa2dfef42f3d0b560a7b7
Nicolas Lafuente <lafuentenicolas1@gmail.com>
2025-07-01 18:14:16 +0200

    feat: added the exercise correction

--- Class/Livre.cs
+++ Class/Livre.cs
@@ -1,12 +1,46 @@
-﻿using System;
+﻿using exercice_de_gestion_des_livres.Class;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace exercice_de_gestion_des_livres.Class
 {
     internal class Livre
     {
+        // propriétés
+        public string Titre { get; set; }
+        public string Auteur { get; set; }
+        public int AnneePublication { get; set; }
+        public bool EstEmprunte { get; set; }
+
+
+        // Constructeur
+        public Livre(string titre, string auteur, int annee, bool emprunt = false)
+        {
+            Titre = titre;
+            Auteur = auteur;
+            AnneePublication = annee;
+            EstEmprunte = emprunt;
+        }
+
+        public void Emprunter() // méthode
+        {
+            EstEmprunte = true;
+            Console.WriteLine($"Le livre {Titre} a été emprunté");
+        }
+
+        public void Rendre() // méthode
+        {
+            EstEmprunte = false;
+            Console.WriteLine($"Le livre {Titre} a été rendu");
+        }
+
+        public void AfficherDetails() // méthode
+        {
+            Console.WriteLine($"{Titre} de {Auteur} en {AnneePublication}. Emprunt:{EstEmprunte}");
+        }
     }
-}
+};
--- Program.cs
+++ Program.cs
@@ -1,10 +1,31 @@
-﻿namespace exercice_de_gestion_des_livres
+﻿using exercice_de_gestion_des_livres.Class;
+
+namespace exercice_de_gestion_des_livres
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Livre premierLivre = new Livre("Harry Potter à l'école des sorciers", "J.K. Rowling", 1997); 
+            Livre deuxiemeLivre = new Livre("La Communauté de l'anneau", "J.R.R. Tolkien", 1954, true);
+            Livre troisiemeLivre = new Livre("L'Étranger", "Albert Camus", 1942, false); // ajout de livre
+
+            premierLivre.AfficherDetails(); // appel des méthodes
+            Console.WriteLine();
+            premierLivre.Emprunter();
+            Console.WriteLine(); // passement de ligne
+            premierLivre.AfficherDetails();
+            Console.WriteLine();
+            premierLivre.Rendre();
+            Console.WriteLine();
+            premierLivre.AfficherDetails();
+            Console.WriteLine();
+            deuxiemeLivre.AfficherDetails();
+            Console.WriteLine();
+            troisiemeLivre.AfficherDetails();
+            Console.WriteLine();
+            premierLivre.AfficherDetails();
         }
     }
-}
+};
