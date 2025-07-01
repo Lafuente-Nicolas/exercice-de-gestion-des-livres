50d3da7f27424df89108617ca9b209744ba5742e
Nicolas Lafuente <lafuentenicolas1@gmail.com>
2025-07-01 11:06:37 +0200

    Ajoutez des fichiers projet.

---
+++ Class/Livre.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercice_de_gestion_des_livres.Class
+{
+    internal class Livre
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace exercice_de_gestion_des_livres
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
